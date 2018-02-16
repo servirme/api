5b6f95c0345348e00a8546157c85582f24faaee4
Denis Bernardo <oi@denisbernardo.com>
2018-02-16 09:27:42 -0200

    New endpoint added: cookery and category

---
+++ src/Controllers/CookeryController.js
@@ -0,0 +1,26 @@
+const cookeryModel = require('../Models/CookeryModel')
+
+module.exports.index = () => {
+  return cookeryModel.list()
+    .then(cookeries => ({
+      statusCode: 200,
+      body: {
+        message: 'cookeries.list',
+        result: cookeries,
+      },
+    }))
+}
+
+module.exports.show = (req) => {
+  const { params } = req
+  const { id } = params
+
+  return cookeryModel.show(id)
+    .then(cookery => ({
+      statusCode: 200,
+      body: {
+        message: 'cookeries.show',
+        result: cookery,
+      },
+    }))
+}
