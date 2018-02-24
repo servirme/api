d74546814a2352984b354bd561a734ce4a85e91d
Matheus Vellone <matheusvellone@hotmail.com>
2018-02-24 10:46:43 -0300

    Copy schema script fix for postgres

--- scripts/copyDatabase.js
+++ scripts/copyDatabase.js
@@ -1,12 +1,10 @@
-// TODO - agora o banco está em postgres, tem q ver se ainda funciona
-// https://stackoverflow.com/questions/18216462/how-to-duplicate-schemas-in-postgresql
-// TODO - tem que testar isso certinho e ver se várias execuções em paralelo não caga
-
 require('../dotenv').config()
 const util = require('util')
 const { exec } = require('child_process')
 const log4js = require('log4js')
+
 require('../config/logger')
+const { DATABASE } = require('../config/constants')
 
 const logger = log4js.getLogger('database')
 const execPromise = util.promisify(exec)
@@ -21,19 +19,18 @@ if (!DATABASE_HOST || !DATABASE_USERNAME || !DATABASE_PASSWORD) {
   throw new Error('Missing some config data')
 }
 
-const BASE_DATABASE = 'servirme'
-const connect = `-port=3306 -u ${DATABASE_USERNAME} -h ${DATABASE_HOST} -p${DATABASE_PASSWORD}`
-const mysqldumpCommand = `mysqldump ${connect} ${BASE_DATABASE}`
+process.env.PGHOST = DATABASE_HOST
+process.env.PGUSER = DATABASE_USERNAME
+process.env.PGPASSWORD = DATABASE_PASSWORD
 
+const pgdumpCommand = `pg_dump --schema='${DATABASE.BASE_CLIENT}' servirme`
+const psqlCommand = 'psql -d servirme'
 const clone = (estabilishmentId) => {
   const newDatabaseName = `servirme_${estabilishmentId}`
+  const replaceDatabaseName = `sed 's/${DATABASE.BASE_CLIENT}/${newDatabaseName}/g'`
 
-  const createDatabase = `CREATE DATABASE \`${newDatabaseName}\`;`
-
-  const mysqlCommandCreateDatabase = `echo '${createDatabase}' | mysql ${connect}`
-  const mysqlCommandCopyDatabase = `${mysqldumpCommand} | mysql ${connect} ${newDatabaseName}`
-
-  return execPromise(`${mysqlCommandCreateDatabase}; ${mysqlCommandCopyDatabase}`)
+  const command = `${pgdumpCommand} | ${replaceDatabaseName} | ${psqlCommand}`
+  return execPromise(command)
     .then(({ stderr }) => {
       if (stderr) {
         logger.error(`Failed to create database for #${estabilishmentId}`, {
