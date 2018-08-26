256ab0894dbf2fa2de09e9e0e1ef2fcdab36c403
Matheus Vellone <matheusvellone@hotmail.com>
2018-08-26 12:23:16 -0300

    Add TODO

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ if (!validEnvs.includes(NODE_ENV)) {
   throw new Error(`Incorrect NODE_ENV '${NODE_ENV}'. Possible values are: ${validEnvs.join(', ')}`)
 }
 
+// TODO: iniciar servidor depois que conexão com o banco estiver OK
+// http://docs.sequelizejs.com/class/lib/sequelize.js~Sequelize.html#instance-method-authenticate
 app.listen(PORT, () => {
   logger.info(`Server is running in '${NODE_ENV}' mode on port ${PORT}`)
 })
