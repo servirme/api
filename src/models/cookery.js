5a40bba35f2e2bca581fdf5a69d1fafb34380648
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2018-09-25 00:24:36 -0300

    remove cookery and category logics

--- src/models/cookery.js
+++
@@ -1,8 +0,0 @@
-const cookeryTransform = require('../transforms/cookery')
-const cookeryRepository = require('../repositories/cookery')
-
-module.exports.list = () => cookeryRepository.paginate()
-  .map(cookeryTransform.output)
-
-module.exports.show = id => cookeryRepository.getOneById(id)
-  .then(cookeryTransform.output)
